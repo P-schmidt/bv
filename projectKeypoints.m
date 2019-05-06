3018dab60f47b8873cb7755b534b7be8f9c17054
Unknown <paul.schmidt@octana.nl>
2019-05-06 10:36:56 +0200

    projectKeypoints werkend
    andere benaming createProj

--- projectKeypoints.m
+++ projectKeypoints.m
@@ -1,18 +1,20 @@
 function projected = projectKeypoints(fa, fb, matches)
 
-xa = fa(1,matches(1,1:4));
-xb = fb(1,matches(2,1:4));
-ya = fa(2,matches(1,1:4));
-yb = fb(2,matches(2,1:4));
-
+% get coordinates for projection matrix
+xb = fb(1,matches(2,3:6));
+ya = fa(2,matches(1,3:6));
+yb = fb(2,matches(2,3:6));
 xy = [xa;ya]';
 xaya = [xb;yb]';
-projMat = createProjectionMatrixE(xy, xaya);
 
+projMat = createProjectionMatrixwk3(xy, xaya);
+
+% perform projection and divide resulting vectors by lambda
+faHomogeneous = [fa(1,matches(1,:));fa(2,matches(1,:)); ones(size(fa(1,matches(1,:))))];
+projected = projMat*faHomogeneous;
+projected = projected(1:2,:)./ projected(3,:)
+
+% ground truth comparison
 fbLocations = [fb(1,matches(2,:));fb(2,matches(2,:))]
 
-faHomogeneous = [fa(1,matches(1,:));fa(2,matches(1,:)); ones(size(fa(1,matches(1,:))))];
-
-% projected = transformPointsForward(projMat, faLocations);
-projected = projMat * faHomogeneous;
 end
