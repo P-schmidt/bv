8f10a2f5e7fd13cfc9747e8b3b3ffd82595a8d0e
Elise Sterk <elisesterk@msn.com>
2019-05-10 16:58:08 +0200

    Goede map

---
+++ ex4/points_of_line.m
@@ -0,0 +1,13 @@
+%Function calculates the distance from points to the houghline.
+%If distance is smaller than epsilon, then that point is appended
+%to the pts list, which gets returned at the end. 
+function [pts] = points_of_line(points, line, epsilon)
+pts = [ ];
+for point = points.'
+    distance = dot(line, point);
+    if distance < epsilon
+        point = point.';
+        pts = [pts; point];
+end
+end
+    
